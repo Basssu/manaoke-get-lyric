d714e266c09b530a14b71feec2904f33ec25efe7
Yuma Ishibashi <yumaishibashi@YumanoMacBook-Air.local>
2023-07-25 21:51:42 +0900

    スキップした動画を返すようにした

--- setVideo/SetVideo.py
+++ setVideo/SetVideo.py
@@ -14,13 +14,16 @@ def inputVideoIds() -> list[str]:
     videoIds = videoIds.split(",")
     return videoIds
 
-def videoIdsLoop(videoIds: list[str], flavor: str, policy: dict):
+def videoIdsLoop(videoIds: list[str], flavor: str, policy: dict) -> list[str]: # 返り値は、スキップされた動画のvideoIdのリスト
+    skippedVideoIds = []
     cf.initializeFirebase(flavor)
     for videoId in videoIds:
-        setEachVideo(videoId, flavor, policy)
+        isNotSkipped = setEachVideo(videoId, flavor, policy)
+        if not isNotSkipped:
+            skippedVideoIds.append(videoId)
         if not cf.answeredYes('次の動画に進みますか？'):
-            unfinishedVideoIds = videoIds[videoIds.index(videoId):]
-            break
+            return skippedVideoIds
+    return skippedVideoIds
 
 def checkCaptionAvailability(videoId: str) -> list[str]:
     try:
@@ -35,12 +38,12 @@ def checkCaptionAvailability(videoId: str) -> list[str]:
             availableLanguages.append('ja')
     return availableLanguages
 
-def setEachVideo(videoId: str, flavor: str, policy: dict):
+def setEachVideo(videoId: str, flavor: str, policy: dict) -> bool: #返り値は、この動画が追加されたかどうか(true: 正常に追加された, false: 追加されなかった)
     availableLanguages = checkCaptionAvailability(videoId)
     if availableLanguages == []: # 日本語・韓国語字幕がない場合
         print(f"{videoId}: この動画には日本語・韓国語字幕どちらもありません")
         firestoreMap = MakeFirestoreMap.makeFirestoreMap(videoId, policy, True, availableLanguages)
-        if cf.answeredYes('この動画をスキップしますか？'): return
+        if cf.answeredYes('この動画をスキップしますか？'): return False
         url = None
     
     if availableLanguages == ['ko']: # 日本語字幕がない場合
@@ -50,7 +53,7 @@ def setEachVideo(videoId: str, flavor: str, policy: dict):
             languages=['ko'],
             )
         firestoreMap = MakeFirestoreMap.makeFirestoreMap(videoId, policy, True, availableLanguages)
-        if cf.answeredYes('この動画をスキップしますか？'): return
+        if cf.answeredYes('この動画をスキップしますか？'): return False
         uncompletedJsonData = convertCaptionsToUncompletedJson(koreanCaptions, None)
         # jsonData = CaptionsToJson.captionsToJson(koreanCaptions, None)
         url = ToStorage.toStorage(f'ko_ja_{videoId}', flavor, uncompletedJsonData, availableLanguages)
@@ -63,7 +66,7 @@ def setEachVideo(videoId: str, flavor: str, policy: dict):
             )
         uncompletedJsonData = convertCaptionsToUncompletedJson(None, japaneseCaptions)
         firestoreMap = MakeFirestoreMap.makeFirestoreMap(videoId, policy, True, availableLanguages)
-        if cf.answeredYes('この動画をスキップしますか？'): return
+        if cf.answeredYes('この動画をスキップしますか？'): return False
         url = ToStorage.toStorage(f'ko_ja_{videoId}', flavor, uncompletedJsonData, availableLanguages)
     
     if 'ja' in availableLanguages and 'ko' in availableLanguages: # 日本語・韓国語字幕がある場合
@@ -79,17 +82,19 @@ def setEachVideo(videoId: str, flavor: str, policy: dict):
         koreanCaptions = deleteIfOneCaptionNotExist(koreanCaptions, japaneseCaptions)
         japaneseCaptions = deleteIfOneCaptionNotExist(japaneseCaptions, koreanCaptions)
         if not cf.answeredYes(f'{videoId}:字幕の行数は{len(koreanCaptions)}行です。続けますか？'):
-            return
+            return False
         jsonData = CaptionsToJson.captionsToJson(koreanCaptions, japaneseCaptions)
         if len(jsonData) != len(koreanCaptions):
             print(f'{videoId}: スクレイプした字幕の行数とjsonの行数が一致しないため、スキップします。')
-            return
+            return False
         firestoreMap = MakeFirestoreMap.makeFirestoreMap(videoId, policy, False, availableLanguages)
-        if cf.answeredYes('この動画をスキップしますか？'): return
+        if cf.answeredYes('この動画をスキップしますか？'): return False
         url = ToStorage.toStorage(f'ko_ja_{videoId}', flavor, jsonData, availableLanguages)
     
     document = ToFireStore.toFirestore(firestoreMap, url, flavor, f'ko_ja_{videoId}', availableLanguages)
     pprint.pprint(document) #Firestoreにアップロードした内容
+
+    return True
 
 def deleteIfOneCaptionNotExist(mainCaptions: list[dict], subCaptions: list[dict]) -> list[dict]:
     captions = []
@@ -135,7 +140,9 @@ def setPolicy() -> dict:
 def main():
     flavor = cf.getFlavor()
     policy = setPolicy()
-    videoIdsLoop(inputVideoIds(), flavor, policy)
+    skippedVideoList = videoIdsLoop(inputVideoIds(), flavor, policy)
+    print(f'スキップした動画の数は{len(skippedVideoList)}です。')
+    print(f'スキップした動画のIDは{skippedVideoList}です。')
 
 if __name__ == '__main__':
     main()
